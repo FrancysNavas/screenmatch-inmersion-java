a3cea0a6152b32bf8ba76259cb929d3c03fbdc43
FrancysNavas <francys.navas.sanchez@gmail.com>
2024-05-05 15:52:56 -0300

    aula-02 - Calculadora de tiempo

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,41 @@
-import java.util.Scanner;
-import java.util.Locale;
+import modelo.Pelicula;
+import modelo.Serie;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("   ****    Bienvenidos a la Inmersión Java!    ****   ");
-        //System.out.println("Película: Matrix");
 
-        //Declaración de variables
-        int fechaDeLanzamiento = 1999;
-        double evaluacion = 4.5;
-        boolean incluidoPlanBasico = true;
-        String nombrePelicula = "Matrix";
-        String sinopsis = """
-                La mejor película del milenio 
-                """;
-        double mediaEvaluacionUsuario = 0.0;
+        Principal principal = new Principal();
+        principal.muestraElMenu();
 
-        System.out.println("-------------------------------------------------------");
-        System.out.println("|                 Screen Match                        |");
-        System.out.println("-------------------------------------------------------");
-        System.out.println("Nombre de la película: "+ nombrePelicula);
-        System.out.println("Fecha de estreno: "+ fechaDeLanzamiento);
-        System.out.println("Sinopsis: "+ sinopsis);
-        System.out.println("Estrellas: "+ evaluacion);
-        System.out.println("Incluido en el Plan Básico: "+ incluidoPlanBasico);
-
-        double mediaEvaluaciones = (4.5 + 4.8 + 3) / 3;
-        System.out.println("Media de la evaluación de Matrix: "+ mediaEvaluaciones);
-
-        if (fechaDeLanzamiento >= 2023){
-            System.out.println("Película popular en el momento");
-        } else {
-            System.out.println("----- Opiniones: Película retro que vale la pena ver");
-        }
-
-        System.out.println("======= Evaluación por parte de usuarios:  ========");
-        /*
-        for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Usuario N° "+(i+1)+ ": Ingresa la nota que le darías a la película: ");
-            double notaMatrix = teclado.nextDouble();
-            mediaEvaluacionUsuario = mediaEvaluacionUsuario + notaMatrix;
-
-        }*/
-
-        int cont = 0;
-        while (cont < 3){
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Usuario N°: Ingresa la nota que le darías a la película: ");
-            double notaMatrix = teclado.nextDouble();
-            cont++;
-            mediaEvaluacionUsuario = mediaEvaluacionUsuario + notaMatrix;
-        }
-
-        System.out.println("La media de la película " + nombrePelicula + " calculada por los usuarios es de: "+ mediaEvaluacionUsuario / 3);
-
+//         Pelicula matrix = new Pelicula();
+//
+//         matrix.setNombre("Matrix");
+//         matrix.setFechaDeLanzamiento(1999);
+//         matrix.setTiempoDuracionEnMinutos(120);
+//         matrix.setIncluidoEnElPlanBasico(true);
+//         matrix.setSinopsis("Mejor película del milenio.");
+//         matrix.muestraFichaTecnica();
+//
+//
+//        Pelicula encanto = new Pelicula();
+//        encanto.setNombre("Encanto");
+//        encanto.setFechaDeLanzamiento(2022);
+//        encanto.setTiempoDuracionEnMinutos(120);
+//        encanto.setIncluidoEnElPlanBasico(true);
+//        encanto.setSinopsis("Una historia familiar muy bonita.");
+//        encanto.muestraFichaTecnica();
+//
+//        Serie friends = new Serie();
+//        friends.setNombre("Friends");
+//        friends.setFechaDeLanzamiento(1996);
+//        friends.setTemporadas(10);
+//        friends.setEpisodiosPorTemporada(22);
+//        friends.setDuracionEnMinutosPorEpisodio(30);
+//        friends.setIncluidoEnElPlanBasico(true);
+//        friends.setSinopsis("La serie más vista de los últimos tiempos.");
+//        friends.muestraFichaTecnica();
 
     }
 }
